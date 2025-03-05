361cd94d3beb5982b0f03555a4125a8207c36864
paulobrun0 <paulo.bruno04@hotmail.com>
2025-03-04 23:08:30 -0300

    Design Patterns & Testes - Gerando coverage de testes

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,6 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
-import { getUserById } from './controllers/getUser'
 
 export async function appRouter(app: FastifyInstance) {
   app.post('/users', register)
-  app.get('/users/:id', getUserById)
 }
--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -3,5 +3,4 @@ import { Prisma, User } from '@prisma/client'
 export interface UsersRepository {
   findByEmail(email: string): Promise<User | null>
   create(data: Prisma.UserCreateInput): Promise<User>
-  // getUserById(id: string): Promise<User | null>
 }
