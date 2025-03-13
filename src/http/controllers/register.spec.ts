c374d388a3edfd38e91ad7eb3232671ed33a25da
paulobrun0 <paulo.bruno04@hotmail.com>
2025-03-13 00:46:58 -0300

    Teste E2E do registro

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,22 @@
-import { test } from 'vitest'
+import { afterEach, beforeAll, describe, expect, it } from 'vitest'
+import { app } from '@/app'
+import request from 'supertest'
 
-test('ok', () => {})
+describe('Register (e2)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterEach(async () => {
+    await app.close()
+  })
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'Paulo Antonio',
+      email: 'paulo.bruno12@hotmail.com',
+      password: '12345678',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
