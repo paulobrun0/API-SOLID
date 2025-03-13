bdcfdaa6225f93090ec4937049770d211bc8e52a
paulobrun0 <paulo.bruno04@hotmail.com>
2025-03-13 14:12:51 -0300

    Refresh Token & RBAC - Testes de RBAC

--- src/Utils/test/create-and-authenticate-user.ts
+++ src/Utils/test/create-and-authenticate-user.ts
@@ -1,11 +1,19 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import { FastifyInstance } from 'fastify'
 import request from 'supertest'
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
-  await request(app.server).post('/users').send({
-    name: 'Paulo Antonio',
-    email: 'paulo.bruno12@hotmail.com',
-    password: '12345678',
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+  isAdmin = false,
+) {
+  await prisma.user.create({
+    data: {
+      name: 'Paulo Antonio',
+      email: 'paulo.bruno12@hotmail.com',
+      password_hash: await hash('12345678', 6),
+      role: isAdmin ? 'ADMIN' : 'MEMBER',
+    },
   })
 
   const authResponse = await request(app.server).post('/sessions').send({
--- src/http/controllers/middleware/verify-user-role.ts
+++ src/http/controllers/middleware/verify-user-role.ts
@@ -1,10 +1,10 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export function verifyUserRole(roleToVerify: 'ADMIN' | 'MEMBER') {
-  return (request: FastifyRequest, reply: FastifyReply) => {
+  return async (request: FastifyRequest, reply: FastifyReply) => {
     const { role } = request.user
 
     if (role !== roleToVerify)
-      return reply.status(401).send({ message: 'Unauthorized' })
+      return reply.status(403).send({ message: 'Unauthorized' })
   }
 }
