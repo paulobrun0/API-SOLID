d84127dc59d6db313d929fa2102930a2e7cce7f7
paulobrun0 <paulo.bruno04@hotmail.com>
2025-03-13 00:53:07 -0300

    Teste E2E authenticate

---
+++ src/http/controllers/authenticate.spec.ts
@@ -0,0 +1,30 @@
+import { afterEach, beforeAll, describe, expect, it } from 'vitest'
+import { app } from '@/app'
+import request from 'supertest'
+
+describe('Authenticate (e2)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterEach(async () => {
+    await app.close()
+  })
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'Paulo Antonio',
+      email: 'paulo.bruno12@hotmail.com',
+      password: '12345678',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'paulo.bruno12@hotmail.com',
+      password: '12345678',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
