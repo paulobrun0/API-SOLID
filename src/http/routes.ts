27c1973658baa6e3a4f4fd58697b24da0bf653b1
paulobrun0 <paulo.bruno04@hotmail.com>
2025-03-12 22:57:07 -0300

    Implementando JWT no Fastify e Controller de perfil

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,13 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
+import { profile } from './controllers/profile'
+import { verifyJWT } from './controllers/middleware/verify-jwt'
 
 export async function appRouter(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  /* Authenticate */
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
