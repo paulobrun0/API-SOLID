e2f73c401b054ffa6843e58b88cdf0cafa5de352
paulobrun0 <paulo.bruno04@hotmail.com>
2025-03-15 01:34:13 -0300

    oi

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client'
 import 'dotenv/config'
 import { execSync } from 'node:child_process'
 import { randomUUID } from 'node:crypto'
-import { Environment } from 'vitest/environments'
+import { Environment } from 'vitest'
 
 const prisma = new PrismaClient()
 
