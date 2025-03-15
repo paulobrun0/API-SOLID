5396a73ddebe581f5874763e92fa4ece5b6ab665
paulobrun0 <paulo.bruno04@hotmail.com>
2025-03-15 16:16:10 -0300

    oi

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    environment: 'prisma',
-    include: ['src/http/controllers/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
-    exclude: [
-      '**/node_modules/**',
-      '**/dist/**',
-      '**/cypress/**',
-      '**/.{idea,git,cache,output,temp}/**',
-      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
-    ],
+    environmentMatchGlobs: [['src/http/controllers/**', 'prisma']],
     dir: 'src',
   },
 })
